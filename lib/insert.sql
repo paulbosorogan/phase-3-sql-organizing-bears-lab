70220f5c0ce5095957b5887083618e67f6b06f8f
Paul Bosorogan <bosorogan.paul@gmail.com>
2023-04-22 17:48:55 -0400

    commit tree 4bba20a94e2f990d0b0b30cbeeabf42156a8abce

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES
+('Mr. Chocolate', 4,'M', 'brown','playful',1),
+('Rowdy',3,'M','grey','sassy',1),
+('Tabitha', 14,'F','brown','calm',0),
+('Sergent Brown', 10,'M', 'brown','calm',1),
+('Melissa', 3,'F', 'white','playful',1),
+('Grinch',11,'M', 'black','sassy',0),
+('Wendy',5,'F','grey','calm',0),
+(NULL, 1, 'F','black','calm', 1);
